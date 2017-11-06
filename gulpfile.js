b2b35c98e17430cc46da0d87675fc83f87b0a187
moran <mofuliunian@gmail.com>
2017-11-06 09:19:41 +0800

    第二次提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var url = require('url');
+var fs = require('fs');
 
-console.log(gulp);
-console.log(webserver);
+gulp.task('webserver',function () {
+    gulp.src('.').pipe(webserver({
+        port: 8080,
+        host: 'localhost',
+        middleware: function (req,res,next) {
+            var obj = url.parse(req.url);
+            res.setHeader('Access-Control-Allow-Origin','*');
+            if(req.method === 'GET'){
+                if(obj.pathname === "/data"){
+                    res.end('1111');
+                }else{
+                    next();
+                }
+            } else {
+                next()
+            }
+        }
+    }))
+})
