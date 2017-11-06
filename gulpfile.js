cabcb675c2df8ad91e7d36e9755415c815f33e09
moran <mofuliunian@gmail.com>
2017-11-06 10:11:47 +0800

    第三次上传

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('webserver',function () {
             res.setHeader('Access-Control-Allow-Origin','*');
             if(req.method === 'GET'){
                 if(obj.pathname === "/data"){
-                    res.end('1111');
+                    res.end('[{"list": [{"icon": "icon-shezhi","xm": "换轮胎","ms": "￥359.00起"}, {"icon": "icon-shezhi","xm": "做保养","ms": "精确匹配"}, {"icon": "icon-shezhi","xm": "洗车","ms": "优质服务"}, {"icon": "icon-shezhi","xm": "领红包","ms": "免单天天送"},],"content": [{"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"img": "images/cen.gif","code":"1"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"}, {"icon": "icon-shezhi","nr": "自助小保养","code":"0"},], "banner":[{"img": "images/banner.gif"}]}]');
                 }else{
                     next();
                 }
